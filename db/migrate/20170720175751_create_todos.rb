3f7709ae5f29d24e6877a9ad8d90c97c0c9c4300
Claire Deng <claireydeng@gmail.com>
2017-07-20 18:01:18 +0000

    new database

--- db/migrate/20170720175751_create_todos.rb
+++ db/migrate/20170720175751_create_todos.rb
@@ -1,7 +1,6 @@
 class CreateTodos < ActiveRecord::Migration
   def change
     create_table :todos do |t|
-      t.integer :id
       t.string :deadline
       t.string :description
       t.string :comments
